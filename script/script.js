b3946b0d9948eb5f81a5c0936815ecbd4a2fbbd6
tahmid <radon1441@gmail.com>
2024-05-16 16:54:44 +0600

    when linked clicked  responsive bar close

--- script/script.js
+++ script/script.js
@@ -1,18 +1,25 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const hamburger = document.querySelector(".hamburger");
-    const navbar = document.querySelector(".navbar");
+  const hamburger = document.querySelector(".hamburger");
+  const navbar = document.querySelector(".navbar");
+  const navLinks = navbar.querySelectorAll("a");
 
-    hamburger.addEventListener("click", function () {
-      navbar.classList.toggle("active");
-    });
+  hamburger.addEventListener("click", function () {
+    navbar.classList.toggle("active");
+  });
 
-    // Close the navbar when clicking outside of it
-    document.addEventListener("click", function (event) {
-      if (
-        !navbar.contains(event.target) &&
-        !hamburger.contains(event.target)
-      ) {
-        navbar.classList.remove("active");
-      }
+  document.addEventListener("click", function (event) {
+    if (
+      !navbar.contains(event.target) &&
+      !hamburger.contains(event.target)
+    ) {
+      navbar.classList.remove("active");
+    }
+  });
+
+  // Add event listener to each navbar link
+  navLinks.forEach(function (link) {
+    link.addEventListener("click", function () {
+      navbar.classList.remove("active");
     });
   });
+});
